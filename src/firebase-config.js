6033b4efcee11e23dbf776665b8cd33c80a58eb0
Jake Wiesler <jakewiesler@gmail.com>
2017-01-05 06:14:55 -0500

    Integrate todos list with firebase using Rebase

---
+++ src/firebase-config.js
@@ -0,0 +1,9 @@
+const Config = {
+  apiKey: "AIzaSyCCToJEX3DZhSrIKWY4PCeFr0qmixIXBB8",
+  authDomain: "todo-react-9a52b.firebaseapp.com",
+  databaseURL: "https://todo-react-9a52b.firebaseio.com",
+  storageBucket: "todo-react-9a52b.appspot.com",
+  messagingSenderId: "305150134793"
+};
+
+export default Config;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import './css/styles.css';
 import TodosList from './components/TodosList';
 import TodoForm from './components/TodoForm';
 import Meta from './components/Meta';
+import Base from './rebase';
 
 class App extends React.Component {
+
   constructor() {
 		super();
 		this.state = {
@@ -17,6 +19,14 @@ class App extends React.Component {
     this.completeTodo = this.completeTodo.bind(this);
     this.clearCompleteTodos = this.clearCompleteTodos.bind(this);
 	}
+
+  componentDidMount() {
+    Base.syncState('todoList', {
+      context: this,
+      state: 'todos',
+      asArray: true
+    });
+  }
 
   addTodo(todo) {
     let todos = this.state.todos;
